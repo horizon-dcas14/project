bb3b815beca5eb62d800a032ce06bdcca34b37b9
ftxsilva <felipetxs@gmail.com>
2019-06-07 16:16:51 +0200

    Create DCGAN
    Create DCGAN

---
+++ DCGAN/DCGAN.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 28 16:16:14 2019
+
+@author: ftxsilva
+"""
+from __future__ import print_function
+#%matplotlib inline
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+from torch.utils.data import Dataset, DataLoader
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+
+# Set random seem for reproducibility
+manualSeed = 999
+#manualSeed = random.randint(1, 10000) # use if you want new results
+print("Random Seed: ", manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
+
+# Root directory for dataset
+dataroot = "../dataset/Data/autonomous_cleaned_normalized/"
+# Number of workers for dataloader
+workers = 2
+# Batch size during training
+batch_size = 128
+# Spatial size of training images. All images will be resized to this
+#   size using a transformer.
+image_size = 64
+# Number of channels in the training data.
+nc = 1
+# Size of z latent vector (i.e. size of generator input)
+nz = 100
+# Size of feature maps in generator
+ngf = 64
+# Size of feature maps in discriminator
+ndf = 10
+# Number of training epochs
+num_epochs = 5
+# Learning rate for optimizers
+lr = 0.0002
+# Beta1 hyperparam for Adam optimizers
+beta1 = 0.5
+# Number of GPUs available. Use 0 for CPU mode.
+ngpu = 1
+
+
+
+"""
+Create the appropriate dataset class format for our problem
+
+"""
+#should we have the header removed?
+def get_data(dataroot):
+    df = pd.read_csv(dataroot)
+    return df.as_matrix()
+
+data_sets = datasets.DatasetFolder(dataroot, 
+                                   loader=get_data, extensions=['.csv'])
+dataloader = torch.utils.data.DataLoader(data_sets,
+                                          batch_size,
+                                          shuffle=False,
+                                          workers)
+
+# Decide which device we want to run on
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+# weights initialization
+# further used in the generator and discriminator
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+# Discriminator architecture
+class Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is (nc=1) x 10 x 3
+            nn.Conv2d(nc, ndf, 3, 1, 0, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+def forward(self, input):
+        return self.main(input)
