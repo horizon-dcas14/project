965deb9e6a7a24c3026726e9b419589c2b50558e
ftxsilva <felipetxs@gmail.com>
2019-06-11 13:54:33 +0200

    Update DCGAN.py

--- DCGAN/DCGAN.py
+++ DCGAN/DCGAN.py
@@ -48,7 +48,7 @@ nz = 100
 # Size of feature maps in generator
 ngf = 64
 # Size of feature maps in discriminator
-ndf = 10
+ndf = 6
 # Number of training epochs
 num_epochs = 5
 # Learning rate for optimizers
@@ -66,7 +66,7 @@ Create the appropriate dataset class format for our problem
 """
 #should we have the header removed?
 def get_data(dataroot):
-    df = pd.read_csv(dataroot)
+    df = pd.read_csv(dataroot, usecols = ['robot_x','robot_y', 'robot_theta'])
     return df.as_matrix()
 
 data_sets = datasets.DatasetFolder(dataroot, 
@@ -89,6 +89,48 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
+# Generator Code
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+    def forward(self, input):
+            return self.main(input)
+        
+# Create the generator
+netG = Generator(ngpu).to(device)
+# Handle multi-gpu if desired
+if (device.type == 'cuda') and (ngpu > 1):
+    netG = nn.DataParallel(netG, list(range(ngpu)))
+# Apply the weights_init function to randomly initialize all weights
+#  to mean=0, stdev=0.2.
+netG.apply(weights_init)
+# Print the model
+print(netG)
+
+
 # Discriminator architecture
 class Discriminator(nn.Module):
     def __init__(self, ngpu):
@@ -99,20 +141,23 @@ class Discriminator(nn.Module):
             nn.Conv2d(nc, ndf, 3, 1, 0, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.Conv2d(ndf, ndf * 2, (3,1), 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            # state size. (ndf*2) x 3 x 1
+            nn.Conv2d(ndf * 2, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
-def forward(self, input):
-        return self.main(input)
+    def forward(self, input):
+            return self.main(input)
+        
+# Create the Discriminator
+netD = Discriminator(ngpu).to(device)
+# Handle multi-gpu if desired
+if (device.type == 'cuda') and (ngpu > 1):
+    netD = nn.DataParallel(netD, list(range(ngpu)))
+# Apply the weights_init function to randomly initialize all weights
+#  to mean=0, stdev=0.2.
+netD.apply(weights_init)
+# Print the model
+print(netD)
